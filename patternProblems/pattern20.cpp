2882fffe5c0a95137fba51b45245c86e9acbf04e
sahitm <sahit7varma@gmail.com>
2022-10-14 08:05:54 +0530

    string pattern butterfly

---
+++ patternProblems/pattern20.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+
+    int z = 1, x = n;
+    while (z<=n)
+    {
+        /* code */
+        int k = 0;
+        while(k<z){
+            cout<<'*';
+            k++;
+        }
+
+        int l = 1;
+        while (l<=2*x-2)
+        {
+            /* code */
+            cout<<" ";
+            l++;
+        }
+        x--;
+
+        int m = 0;
+        while(m<z){
+            cout<<'*';
+            m++;
+        }
+        
+        z++;
+        cout<<'\n';
+    }
+
+
+    int i = 0;
+    while (i<n)
+    {
+        /* code */
+        int k = n-i;
+        while (k>0)
+        {
+            /* code */
+            cout<<"*";
+            k--;
+        }
+
+        int l = 0;
+        while(l<2*i){
+            cout<<' ';
+            l++;
+        }
+
+        int m = n-i;
+        while (m>0)
+        {
+            /* code */
+            cout<<"*";
+            m--;
+        }
+        
+        cout<<"\n";
+        i++;
+    }    
+    
+}
+
+int main(){
+    pattern(5);
+}
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
