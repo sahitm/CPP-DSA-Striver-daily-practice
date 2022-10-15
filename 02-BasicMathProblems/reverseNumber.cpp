ab1f7762464667a05727c7b761803fb7d6def18e
sahitm <sahit7varma@gmail.com>
2022-10-15 17:02:17 +0530

    reverse the number

---
+++ 02-BasicMathProblems/reverseNumber.cpp
@@ -0,0 +1,27 @@
+// Given a number N reverse the number and print it.
+#include<bits/stdc++.h>
+using namespace std;
+
+int printReverse(long long x){
+    string s = to_string(x);
+    string reverse = "";
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        /* code */
+        reverse = s[i]+reverse;
+    }
+
+    long long stingtoIntger = stoll(reverse) ;
+
+    if(x<0){
+        return (stingtoIntger<INT_MIN || stingtoIntger>INT_MAX) ? 0 : -stingtoIntger;
+    }else{
+        return (stingtoIntger<INT_MIN || stingtoIntger>INT_MAX) ? 0 : stingtoIntger;
+    }
+    
+}
+
+int main(){
+    cout<<"The reverse of the number is :"<<printReverse(1534236469);
+}
